ec2569fe1a44fcc7dfe78f27e618d6ac61a49917
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2024-10-14 11:52:18 -0400

    Adicion pruebas unitarias para AI_classes.py

--- test/test_game_control.py
+++ test/test_game_control.py
@@ -4,7 +4,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from game_control import *
-
+from AI_classes import *
 
 @pytest.mark.parametrize(
         "event, expected",
@@ -43,3 +43,24 @@ def test_select_move_color(select_L, select_R,selected,result):
 )
 def test_player_LR_selection_color(selected,result):
     assert player_LR_selection_color(selected) == result
+
+
+def test_leaf_initialization():
+    leaf = Leaf(10)
+    assert leaf.value == 10
+
+def test_branch_initialization():
+    leaf1 = Leaf(1)
+    leaf2 = Leaf(2)
+    branch = Branch(Branch_q="Is it a leaf?", child_1=leaf1, child_2=leaf2)
+    
+    assert branch.question == "Is it a leaf?"
+    assert branch.child_1 == leaf1
+    assert branch.child_2 == leaf2
+
+def test_get_offshoots():
+    leaf1 = Leaf(1)
+    leaf2 = Leaf(2)
+    branch = Branch(child_1=leaf1, child_2=leaf2)
+    
+    assert branch.get_offshoots() == (leaf1, leaf2)

32a160c401c56fbd19dd819a01c077483256b965
Elian Tito <eliantito234@gmail.com>
2024-10-13 20:12:27 -0400

    Agregacion de 4 pruebas para la funcion get_keypress del archivo game_control

--- test/test_game_control.py
+++ test/test_game_control.py
@@ -1,6 +1,23 @@
 import pytest
+import pygame
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from game_control import *
 
+
+@pytest.mark.parametrize(
+        "event, expected",
+        [
+            (pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT),(True, False, False)),
+            (pygame.event.Event(pygame.KEYDOWN, key=pygame.K_RIGHT),(False, True, False)),
+            (pygame.event.Event(pygame.KEYDOWN, key=pygame.K_UP),(False, False, True))
+        ]
+)
+
+def test_get_keypress(event, expected):
+    assert get_keypress(event) == expected
+
+
+def test_select_move_color():
+    pass

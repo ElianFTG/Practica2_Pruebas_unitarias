6019be669285a5d328be4b55b66feb5476b476ee
Elian <eliantito234@gmail.com>
2024-10-16 09:16:12 -0400

    pruebas aniadidas para la funcion player_LR_selection_hand

--- test/test_game_control.py
+++ test/test_game_control.py
@@ -4,7 +4,8 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from game_control import *
-from AI_classes import *
+from game_classes import Player
+
 
 @pytest.mark.parametrize(
         "event, expected",
@@ -51,8 +52,50 @@ def test_player_LR_selection_color(selected,result):
             (1,0,[1,1,1,1],None,0),
             (0,1,[1,1],2,1),
             (0,1,[1,1,1,1],None,1)
-
         ]
 )
 def test_select_move_target(select_L, select_R, players, selected,result):
     assert select_move_target(select_L, select_R, players, selected) == result
+
+
+@pytest.mark.parametrize(
+        "select_L, select_R, allowed_card_list, selected, result",
+        [
+            (1,0,["y","b","r"],None,0),
+            (0,1,["y","b"],2,1),
+            (0,1,["y","b","r"],None,1)
+        ]
+)
+def test_select_move_hand(select_L, select_R, allowed_card_list, selected, result):
+    assert select_move_hand(select_L, select_R, allowed_card_list, selected) == result
+
+
+def test_player_LR_selection_hand_no_board_no_allowed():
+    player = Player(name="Test Player")
+    selected = "Card1"
+    result = player_LR_selection_hand(player, selected)
+    assert result == (0,"Card1",0)  # Reemplaza con el resultado esperado
+
+# Test when allowed_card_list contains the selected card
+def test_player_LR_selection_hand_card_allowed():
+    player = Player(name="Test Player")
+    selected = "Card1"
+    allowed_card_list = ["Card1", "Card3"]
+    result = player_LR_selection_hand(player, selected, allowed_card_list=allowed_card_list)
+    assert result ==  (False, 'Card1', False) # Reemplaza con el resultado esperado
+
+# Test when selected card is not in allowed_card_list
+def test_player_LR_selection_hand_card_not_allowed():
+    player = Player(name="Test Player")
+    selected = "Card2"
+    allowed_card_list = ["Card1"]
+    result = player_LR_selection_hand(player, selected, allowed_card_list=allowed_card_list)
+    assert result == (False, 'Card2', False)  # Reemplaza con el resultado esperado
+
+# Test when player has no valid cards in hand
+def test_player_LR_selection_hand_no_valid_cards():
+    player = Player(name="Test Player")
+    selected = None
+    allowed_card_list = ["Card1"]
+    result = player_LR_selection_hand(player, selected, allowed_card_list=allowed_card_list)
+    assert result == (False, None, False)
